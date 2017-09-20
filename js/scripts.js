3cf422ff92c3505c3661e3dc322f47c14be17906
Kimberly Bordon <kbordon@gmail.com>
2017-09-19 21:15:35 -0700

    Adjust display of output.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ var leapYear = function(year) {
   } else {
     return false;
   }
+}
 
 
 // jquery user interface logic here.
@@ -12,6 +13,14 @@ $(document).ready(function() {
     event.preventDefault();
     var year= parseInt($("input#year").val());
     var result = leapYear(year);
+    $(".year").text(year);
+
+    if (!result) {            // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
     $("#result").text(result);
   });
 });
